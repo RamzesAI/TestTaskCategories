0c079ff59fc29499da9053221a118efe046ca5ae
Aksanov Roman <ramzesru7@yandex.ru>
2023-04-13 16:32:39 +0300

    join metods

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -8,34 +8,54 @@ import {
   Param,
   Delete,
   UsePipes,
+  Query,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+
+export interface GetValueParams {
+  id?: string;
+  slug?: string;
+}
+
+export interface GetFilterParams {
+  name?: string;
+  description?: string;
+  active?: boolean | string;
+}
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @UsePipes(ValidationPipe)
-  @Post('create')
+  @Post()
   create(@Body() dto: CreateCategoryDto) {
     return this.categoryService.create(dto);
   }
 
-  @Get('get-all')
+  @Get()
   findAll() {
     return this.categoryService.findAll();
   }
 
-  @Get('get-by-id/:id')
-  findById(@Param('id') id: string) {
-    return this.categoryService.findById(id);
+  @Get('get-by-value')
+  findByValue(@Query() value: GetValueParams) {
+    return this.categoryService.findByValue(value);
   }
 
-  @Get('get-by-slug/:slug')
-  findBySlug(@Param('slug') slug: string) {
-    return this.categoryService.findBySlug(slug);
+  @Get('get-by-filter')
+  findByFilter(
+    @Query('name') name: string,
+    @Query('description') description: string,
+    @Query('active') active: string,
+    @Query('search') search: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+    @Query('sort') sort: string,
+  ) {
+    return this.categoryService.findByFilter();
   }
 
   @Patch('update/:id')
