d430886d6a93be1198ed9ffd7a802c1893a46649
Aksanov Roman <ramzesru7@yandex.ru>
2023-04-14 10:47:53 +0300

    add ilike

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -13,6 +13,7 @@ import {
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { QueryParams } from './dto/aboba';
 
 export interface GetValueParams {
   id?: string;
@@ -30,35 +31,22 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @UsePipes(ValidationPipe)
-  @Post()
+  @Post() // create category
   create(@Body() dto: CreateCategoryDto) {
     return this.categoryService.create(dto);
   }
 
-  @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  @Get('/all') // get all categories
+  findByFilter(@Query() query: QueryParams) {
+    return this.categoryService.findByFilter(query);
   }
 
-  @Get('get-by-value')
+  @Get() // get category by id or slug
   findByValue(@Query() value: GetValueParams) {
     return this.categoryService.findByValue(value);
   }
 
-  @Get('get-by-filter')
-  findByFilter(
-    @Query('name') name: string,
-    @Query('description') description: string,
-    @Query('active') active: string,
-    @Query('search') search: string,
-    @Query('page') page: number,
-    @Query('pageSize') pageSize: number,
-    @Query('sort') sort: string,
-  ) {
-    return this.categoryService.findByFilter();
-  }
-
-  @Patch('update/:id')
+  @Patch('/:id')
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -66,7 +54,7 @@ export class CategoryController {
     return this.categoryService.update(id, updateCategoryDto);
   }
 
-  @Delete('delete/:id')
+  @Delete('/:id')
   remove(@Param('id') id: string) {
     return this.categoryService.remove(id);
   }
---
+++ src/category/dto/aboba.ts
@@ -0,0 +1,9 @@
+export class QueryParams {
+  name?: string;
+  description?: string;
+  active?: boolean;
+  search?: string;
+  page?: number;
+  pageSize?: number;
+  sort?: string;
+}
